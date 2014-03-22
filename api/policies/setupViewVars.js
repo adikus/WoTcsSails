485865b514f997e8dec5f3ea95957894bcef1ead
Andrej Hoos <andrej.hoos@gmail.com>
2014-03-22 13:40:22 +0000

    Navbar is now responsive

--- api/policies/setupViewVars.js
+++ api/policies/setupViewVars.js
@@ -2,6 +2,9 @@ var _ = require('underscore');
 
 module.exports = function(req, res, next) {
 
+    if(!res.vars){
+        res.vars = {};
+    }
     res.vars = _(res.vars).extend(req.target);
     next();
 
